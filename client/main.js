98e8000e2ae5d0445adf03e44792addd898e0931
ksnt <ksn0215@gmail.com>
2018-11-05 06:10:42 +0900

    reindent, remove unused parts

--- client/main.js
+++ client/main.js
@@ -7,66 +7,49 @@ import './main.html';
 /// routing
 
 Router.configure({
-	layoutTemplate: 'ApplicationLayout'
+  layoutTemplate: 'ApplicationLayout'
 });
 
 Router.route('/', function () {
-  this.render('nothing',{
-  	to:"navbar"
+  this.render('nothing', {
+    to: "navbar"
   });
-  this.render('welcome',{
-  	to:"top"
+  this.render('welcome', {
+    to: "top"
   });
-  this.render('nothing',{
-  	to:"main"
+  this.render('nothing', {
+    to: "main"
   });
 });
 
 Router.route('/items', function () {
-  this.render('navbar',{
-  	to:"navbar"
+  this.render('navbar', {
+    to: "navbar"
   });
-  this.render('items',{
-  	to:"top"
+  this.render('items', {
+    to: "top"
   });
-  this.render('canvas',{
-  	to:"main"
+  this.render('canvas', {
+    to: "main"
   });
-  this.render('inputForm',{
-    to:"bottom"
+  this.render('inputForm', {
+    to: "bottom"
   });
-  this.render('commentList',{
-    to:"rightspace"
+  this.render('commentList', {
+    to: "rightspace"
   });
 });
 
-Router.route('/conversation', function () {
-    this.render('navbar',{
-  	  to:"navbar"
-    });
-    this.render('items',{
-  	  to:"top"
-    });
-    this.render('canvas',{
-  	  to:"main"
-    });
-    this.render('inputForm',{
-  	  to:"bottom"
-    });
-    this.render('commentList',{
-  	  to:"rightspace"
-    });
-});
 
 Router.route('/manual', function () {
-  this.render('navbar',{
-  	to:"navbar"
+  this.render('navbar', {
+    to: "navbar"
   });
-  this.render('nothing',{
-  	to:"top"
+  this.render('nothing', {
+    to: "top"
   });
-  this.render('manual',{
-  	to:"main"
+  this.render('manual', {
+    to: "main"
   });
 });
 
@@ -75,119 +58,90 @@ Template.canvas.onRendered(function () {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var color = "black"
-  //ctx.fillStyle = color;
-  //ctx.fillRect(10, 10, 100, 100);
-  //ctx.font = "48px serif";
-  //ctx.fillText("What is your idea?", 10, 50);
- 
-  /*
-  function onDown(e) {
-    console.log("down");
-  }
-  canvas.addEventListener('mousedown', onDown, false);
-
-  function onClick(e) { // get the (x,y) point
-    console.log("click");
-    var x = e.clientX - canvas.offsetLeft;
-    var y = e.clientY - canvas.offsetTop;
-    console.log("x:", x, "y:", y);
-
-    drawRect(x, y, 10, 10);
-  }
-
-  function drawRect(x, y, width, height) {
-    var context = canvas.getContext('2d');
-    context.fillRect(x, y, width, height);
-  }
-
-  canvas.addEventListener('click', onClick, false);
-  */
-
 
   //マウスを操作
-   var mouse = {x:0,y:0,x1:0,y1:0,color:"black"};
-   var draw = false;
+  var mouse = { x: 0, y: 0, x1: 0, y1: 0, color: "black" };
+  var draw = false;
 
-   //マウスの座標を取得する
-   canvas.addEventListener("mousemove",function(e) {
-     var rect = e.target.getBoundingClientRect();
-     ctx.lineWidth = document.getElementById("lineWidth").value;
-     ctx.globalAlpha = document.getElementById("alpha").value/100;
-     //ctx.lineWidth = 10
-     //ctx.globalAlpha = 100
+  //マウスの座標を取得する
+  canvas.addEventListener("mousemove", function (e) {
+    var rect = e.target.getBoundingClientRect();
+    ctx.lineWidth = document.getElementById("lineWidth").value;
+    ctx.globalAlpha = document.getElementById("alpha").value / 100;
+    //ctx.lineWidth = 10
+    //ctx.globalAlpha = 100
 
-     mouseX = e.clientX - rect.left;
-     mouseY = e.clientY - rect.top;
-   
-     //クリック状態なら描画をする
-     if(draw === true) {
-       ctx.beginPath();
-       ctx.moveTo(mouseX1,mouseY1);
-       ctx.lineTo(mouseX,mouseY);
-       ctx.lineCap = "round";
-       ctx.stroke();
-       mouseX1 = mouseX;
-       mouseY1 = mouseY;
-       }
+    mouseX = e.clientX - rect.left;
+    mouseY = e.clientY - rect.top;
+
+    //クリック状態なら描画をする
+    if (draw === true) {
+      ctx.beginPath();
+      ctx.moveTo(mouseX1, mouseY1);
+      ctx.lineTo(mouseX, mouseY);
+      ctx.lineCap = "round";
+      ctx.stroke();
+      mouseX1 = mouseX;
+      mouseY1 = mouseY;
+    }
   });
 
   //クリックしたら描画をOKの状態にする
-  canvas.addEventListener("mousedown",function(e) {
+  canvas.addEventListener("mousedown", function (e) {
     draw = true;
     mouseX1 = mouseX;
     mouseY1 = mouseY;
-    undoImage = ctx.getImageData(0, 0,canvas.width,canvas.height);
+    undoImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
   });
 
   //クリックを離したら、描画を終了する
-  canvas.addEventListener("mouseup", function(e){
+  canvas.addEventListener("mouseup", function (e) {
     draw = false;
   });
 
   //線の太さの値を変える
-  lineWidth.addEventListener("mousemove",function(){  
+  lineWidth.addEventListener("mousemove", function () {
     var lineNum = document.getElementById("lineWidth").value;
     document.getElementById("lineNum").innerHTML = lineNum;
   });
 
   //透明度の値を変える
-  alpha.addEventListener("mousemove",function(){  
-  var alphaNum = document.getElementById("alpha").value;
-  document.getElementById("alphaNum").innerHTML = alphaNum;
+  alpha.addEventListener("mousemove", function () {
+    var alphaNum = document.getElementById("alpha").value;
+    document.getElementById("alphaNum").innerHTML = alphaNum;
   });
 
   //消去ボタンを起動する
- $('#clear').click(function(e) {
- 		if(!confirm('Do you really clean up?')) return;
-        e.preventDefault();
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+  $('#clear').click(function (e) {
+    if (!confirm('Do you really clean up?')) return;
+    e.preventDefault();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   });
-    
-//戻るボタンを配置
-$('#undo').click(function(e) {
-    ctx.putImageData(undoImage,0,0);
-});
 
-//保存する
-//function save(){
-$('#save').click(function(e) {
-  /*var can = canvas.toDataURL();
-  can = can.replace("image/png", "image/octet-stream");
-  window.open(can,"save");*/
-  var imageType = "image/png";
-  var fileName = "image.png";
-  var canvas = document.getElementById("canvas");
-   // base64エンコードされたデータを取得 「data:image/png;base64,iVBORw0k～」
-   var base64 = canvas.toDataURL(imageType);
-   // base64データをblobに変換
-   var blob = Base64toBlob(base64);
-   // blobデータをa要素を使ってダウンロード
-   saveBlob(blob, fileName);
- });
+  //戻るボタンを配置
+  $('#undo').click(function (e) {
+    ctx.putImageData(undoImage, 0, 0);
+  });
 
-// Base64データをBlobデータに変換
-function Base64toBlob(base64)
-{
+  //保存する
+  //function save(){
+  $('#save').click(function (e) {
+    /*var can = canvas.toDataURL();
+    can = can.replace("image/png", "image/octet-stream");
+    window.open(can,"save");*/
+    var imageType = "image/png";
+    var fileName = "image.png";
+    var canvas = document.getElementById("canvas");
+    // base64エンコードされたデータを取得 「data:image/png;base64,iVBORw0k～」
+    var base64 = canvas.toDataURL(imageType);
+    // base64データをblobに変換
+    var blob = Base64toBlob(base64);
+    // blobデータをa要素を使ってダウンロード
+    saveBlob(blob, fileName);
+  });
+
+  // Base64データをBlobデータに変換
+  function Base64toBlob(base64) {
     // カンマで分割して以下のようにデータを分ける
     // tmp[0] : データ形式（data:image/png;base64）
     // tmp[1] : base64データ（iVBORw0k～）
@@ -195,20 +149,19 @@ function Base64toBlob(base64)
     // base64データの文字列をデコード
     var data = atob(tmp[1]);
     // tmp[0]の文字列（data:image/png;base64）からコンテンツタイプ（image/png）部分を取得
-	var mime = tmp[0].split(':')[1].split(';')[0];
+    var mime = tmp[0].split(':')[1].split(';')[0];
     //  1文字ごとにUTF-16コードを表す 0から65535 の整数を取得
-	var buf = new Uint8Array(data.length);
-	for (var i = 0; i < data.length; i++) {
-        buf[i] = data.charCodeAt(i);
+    var buf = new Uint8Array(data.length);
+    for (var i = 0; i < data.length; i++) {
+      buf[i] = data.charCodeAt(i);
     }
     // blobデータを作成
-	var blob = new Blob([buf], { type: mime });
+    var blob = new Blob([buf], { type: mime });
     return blob;
-}
+  }
 
-// 画像のダウンロード
-function saveBlob(blob, fileName)
-{
+  // 画像のダウンロード
+  function saveBlob(blob, fileName) {
     var url = (window.URL || window.webkitURL);
     // ダウンロード用のURL作成
     var dataUrl = url.createObjectURL(blob);
@@ -223,49 +176,49 @@ function saveBlob(blob, fileName)
     a.download = fileName;
     // イベントの発火
     a.dispatchEvent(event);
-}
+  }
 
- //色を選択
- $('li').click(function() {
-        ctx.strokeStyle = $(this).css('background-color');
- });
+  //色を選択
+  $('li').click(function () {
+    ctx.strokeStyle = $(this).css('background-color');
+  });
 
 }); // / of Template.canvas.onRendered
 
 if (Meteor.isClient) {
   /// accounts config
-	Accounts.ui.config({
+  Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
 
-  Template.items.helpers({username:function(){
-  	if(Meteor.user()){
-  		return Meteor.user().username;
-  	}
-  	else{
-  		return "anonymous internet user";
-  	}
-  }
-
+  Template.items.helpers({
+    username: function () {
+      if (Meteor.user()) {
+        return Meteor.user().username;
+      }
+      else {
+        return "anonymous internet user";
+      }
+    }
   });
-  
+
   //コメントを取得
-    Template.commentList.helpers({
+  Template.commentList.helpers({
     'comment': function () {
-        return Comments.find();
+      return Comments.find();
     }
   });
 
   //入力されたコメントを取得し、用意されたCollectionに挿入している
   Template.inputForm.events({
     'submit form': function (event) {
-        event.preventDefault();
-        var cmt = event.target.inputComment.value;
-        var eMail = Meteor.user().emails[0].address;
-        Comments.insert({
-          commentStr:cmt,
-          userStr:eMail
-        });
+      event.preventDefault();
+      var cmt = event.target.inputComment.value;
+      var eMail = Meteor.user().emails[0].address;
+      Comments.insert({
+        commentStr: cmt,
+        userStr: eMail
+      });
     }
   });
 }
